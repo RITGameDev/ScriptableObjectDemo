6691d7ef8c5c0865a922ecd08f1394ad348857d3
Ben Hoffman <benjamin.hoffman.dev@gmail.com>
2018-06-27 17:11:13 -0400

    First project commit

---
+++ Assets/Scripts/PickupDisplay.cs
@@ -0,0 +1,24 @@
+﻿
+using UnityEngine;
+
+/// <summary>
+/// Example of how you could use a scriptable object from 
+/// Inside a MonoBehavior
+/// 
+/// Author: Ben Hoffman 
+/// 6/27/2018
+/// </summary>
+public class PickupDisplay : MonoBehaviour
+{
+    /// <summary>
+    /// The PickupItem scriptable object data to use on this display
+    /// </summary>
+    public PickupItem Pickup;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+}
---
+++ Assets/Scripts/PickupItem.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Template for a pickup item in our game
+/// This is an example of how you can use scriptable objects
+/// 
+/// Author: Ben Hoffman
+/// 6/27/2018
+/// </summary>
+[CreateAssetMenu(fileName = "Pickup Item", menuName = "Pickup")]
+public class PickupItem : ScriptableObject
+{
+    /// <summary>
+    /// The name of this pickup
+    /// </summary>
+    public string PickupName;
+
+    /// <summary>
+    /// Description of what this item does
+    /// </summary>
+    public string Description;
+
+    /// <summary>
+    /// Color of this pickup
+    /// </summary>
+    public Color PickupColor;
+
+    /// <summary>
+    /// How much does this item weigh?
+    /// </summary>
+    public int Weight;
+    
+}
