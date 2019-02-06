2d5b3968814de4a0bfa57afc2c3f2b2af35dba0e
Ben Hoffman <benjamin.hoffman.dev@gmail.com>
2019-02-05 19:41:14 -0500

    Live demo boi

---
+++ Assets/Scripts/Example_ScriptableObject.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public enum EGunType
+{
+    rifle,
+    pistol,
+    grenade,
+    sniper
+}
+
+[CreateAssetMenu(fileName = "Weapons", menuName = "Gun Type")]
+public class Example_ScriptableObject : ScriptableObject
+{
+    public EGunType GunType;
+
+    public CompleteDemo.PickupItem AmmoType;
+
+    [Range(1, 10)]
+    public int damage = 10;
+
+    public Material gunMat;
+}
---
+++ Assets/Scripts/Use_Excriptableobject.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class Use_Excriptableobject : MonoBehaviour
+{
+    public Example_ScriptableObject Gun;
+
+
+    void Update()
+    {      
+        if(Input.GetKeyDown(KeyCode.A))
+        {
+            switch(Gun.GunType)
+            {
+                case EGunType.grenade:
+                    Debug.Log("BOOM");
+                    break;
+
+                case EGunType.rifle:
+                    Debug.Log("<color=red>BANG BOI</color>");
+                    break;
+
+                default:
+                    break;
+            }
+        }
+    }
+}
